7f1e1d9d32448f1f0437a94e61c303930b704b99
Patrick Watson <watson81@users.noreply.github.com>
2017-01-15 01:39:16 +0000

    Move the build script out into a build subfolder
    Having the build script external to the .travis.yml file makes it
    easier for me to start local builds.

---
+++ build/main.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -ev
+
+if [ "${TRAVIS_TAG}" == "" ]; then
+	export VERSION=`git rev-parse --abbrev-ref HEAD`;
+else
+	export VERSION=${TRAVIS_TAG:1};
+fi;
+if [ "${VERSION}" == "HEAD" ]; then
+	export VERSION=master;
+fi;
+
+export BUILD=`git rev-parse HEAD`;
+
+echo ${VERSION} ${BUILD}
+
+make build
+make test
+make dist
